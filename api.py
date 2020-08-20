1ea35f1ef124d10c19555c2e6ffc62b6be1bd194
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2020-08-20 09:27:09 +0000

    hi

---
+++ api.py
@@ -0,0 +1,168 @@
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+import pickle
+import csv
+import numpy as np
+import json
+import pandas as pd
+import spacy
+import psycopg2
+import api_helpers
+
+app = Flask(__name__)
+
+# utility function: sample from an arbitrary discrete distribution
+
+def sample_discrete(weights):
+
+    weights = weights/np.sum(weights)
+
+    x = np.random.rand()
+    sum = weights[0]
+    i = 0
+
+    while sum < x:
+        sum += weights[i+1]
+        i += 1
+
+    return i
+        
+
+# compute the correct probability values
+
+# utility function: load a word from a CSV with a score in a particular range.
+
+def word_range(lower, upper):
+    print(lower)
+    with open("vocab2.csv", 'r') as vocabfile:
+        reader = list(csv.reader(vocabfile))
+        for i,line in enumerate(reader[1:]):
+            if float(line[2]) < upper:
+                upperid = i
+                break
+        for i,line in enumerate(reader[upperid+1:]):
+            if float(line[2]) < lower:
+                print(line[2])
+                lowerid = i
+                break
+        print("lowerid")
+        print(lowerid)
+        print("upperid")
+        print(upperid)
+        n = np.random.randint(1+upperid, 1+lowerid+upperid)
+        return reader[n][0], reader[n][2]
+                
+
+def word_computation(data):
+
+    if data["number"] == 0:
+        N = np.random.randint(19000)
+    
+        with open("vocab2.csv", 'r') as vocabfile:
+            reader = csv.reader(vocabfile)
+            line = next((x for i, x in enumerate(reader) if i == N), None)
+
+        
+        word = line[0]
+        score = line[2]
+
+    else:
+        with open("temp_data.csv", 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            rows = []
+            sortedlist = sorted(reader, key=lambda x: x[2])
+            for row in sortedlist:
+                if row[0] == data["userId"]:
+                    rows.append(row)
+
+        scores = np.array([float(row[2]) for row in rows])
+        answered = np.array([float(row[3]) for row in rows])
+
+        vals = np.zeros(len(scores)+1)
+        weights = []
+        for j in range(1,8):
+            newanswered = []
+            for i,answer in enumerate(answered):
+                if scores[i] < j:
+                    newanswered.append(1 - answer)
+                else: newanswered.append(answer)
+            newanswered = np.array(newanswered)
+            newanswered = 0.8*newanswered + 0.2*(1-newanswered)
+            weights.append(np.prod(newanswered))
+        x = sample_discrete(weights) + 1
+
+        #changes = [19297, 19025, 15396, 7101, 1524, 138, 12]
+        # optimise this later
+        
+
+
+        # divide into integer sections. compute likelihood of falling into each integer section, and
+        # sample accordingly (using the direct values).
+
+        
+        word, score = word_range(x, x+1)
+
+    return word, score
+
+def select_word(stuff):
+
+    word, score = word_computation(stuff)
+
+    return word, score
+
+@app.route('/api/getword', methods=["POST", "GET"])
+def get_word():
+
+    req = request.get_json()
+
+    if req["number"] > 0:
+        with open("temp_data.csv", 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([req["userId"], req["word"], req["score"], req["answered"]])
+
+    new_word, new_score = select_word(req)
+
+    res = make_response(jsonify({"word": new_word, "score": new_score}))
+
+    return res
+
+@app.route('/api/getchunk', methods=["POST", "GET"])
+def get_text_chunk():
+    
+    conn = psycopg2.connect("dbname=ll user=postgres password=postgres")
+
+    cur = conn.cursor()
+
+    req = request.get_json()
+    
+    userid = req["userid"]
+    
+    if req["answeredCorrect"] == -1:
+        out = api_helpers.next_chunk(userid, cur)
+        
+    else:
+        
+        # record the results
+        
+        api_helpers.record_result(userid, req, cur)
+        out = api_helpers.next_chunk(userid, cur)
+            
+
+    res = make_response(jsonify(out))
+    
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    return res
+
+@app.route('/api/dumpresult', methods=["POST", "GET"])
+def dump_result():
+    req = request.json
+
+    with open("temp_data", 'wb') as dumpfile:
+        pickle.dump(req, dumpfile)
+
+    res = make_response()
+
+    return res
---
+++ api_helpers.py
@@ -0,0 +1,297 @@
+import numpy as np
+import random
+import psycopg2
+
+def get_grammar(chunkid, cur):
+
+    COMMAND = """
+    SELECT g.id, g.item, cg.locations FROM grammar g
+    INNER JOIN chunk_grammar cg
+    ON g.id = cg.grammar_id
+    WHERE cg.chunk_id=%s
+    """
+    cur.execute(COMMAND, (chunkid,))
+    return(cur.fetchall())
+
+def get_vocab(chunkid, cur):
+
+    COMMAND = """
+    SELECT v.id, v.word, cv.locations, v.pos, cv.sentences FROM vocab v
+    INNER JOIN chunk_vocab cv
+    ON v.id = cv.vocab_id
+    WHERE cv.chunk_id=%s
+    """
+    cur.execute(COMMAND, (chunkid,))
+    return(cur.fetchall())
+
+def get_vocab_interaction_data(chunkid, cur, vocab, interaction):
+    interaction_data = []
+    for idx, v in enumerate(vocab):
+        
+        COMMAND = """
+        SELECT v.pos, v.zipf, v.word, uv.streak FROM vocab v
+        INNER JOIN user_vocab uv
+        ON uv.vocab_id = v.id
+        WHERE v.id = %s
+        """
+        cur.execute(COMMAND, (v,))
+        
+        # WHILE NOT ALL HAVE STREAKS.
+        out = cur.fetchall()[0]
+        pos = out[0]; zipf = out[1]; wd = out[2];
+        if len(out) > 2: 
+            streak = out[3]
+        else:
+            streak = 0
+         
+        outdata = {}
+        outdata["pos"] = pos
+        outdata["zipf"] = zipf
+        outdata["wd"] = wd
+        outdata["streak"] = streak
+        
+        if interaction[idx]=="1":
+
+            COMMAND = """
+            SELECT c.id, c.chunk, cv.locations FROM chunks c
+            INNER JOIN chunk_vocab cv
+            ON c.id = cv.chunk_id
+            WHERE cv.vocab_id=%s AND c.id !=%s AND c.sentence=1
+            """
+            cur.execute(COMMAND, (v,chunkid))
+            options = cur.fetchall()
+            outdata["raw"] = random.sample(options, min(2, len(options)))
+        
+        if interaction[idx]=="2":
+            
+            COMMAND = """
+            SELECT v.word, v.zipf FROM vocab v
+            INNER JOIN synonyms s
+            ON s.vocab2_id = v.id
+            WHERE s.vocab1_id=%s
+            """
+            cur.execute(COMMAND, (v,))
+            options = cur.fetchall()
+            options.sort(key=lambda x: x[1])
+            outdata["raw"] = options[max(-len(options), -3):]
+            
+        if interaction[idx]=="3":
+            
+            COMMAND = """
+            SELECT word, zipf FROM vocab
+            WHERE pos=%s AND ABS(id - %s) < 200 AND id != %s
+            """
+            cur.execute(COMMAND, (pos, v, v))
+            options = cur.fetchall()
+            options.sort(key=lambda x: np.abs(x[1] - zipf))
+            y = options[:min(len(options), 3):]
+            y.append((wd, zipf))
+            random.shuffle(y)
+            outdata["raw"] = y
+            
+        interaction_data.append(outdata)
+                        
+    return interaction_data
+
+def next_chunk(user, cur, next_interaction=0, chunkid=None):
+    
+    if not chunkid:
+    
+        COMMAND = """
+        SELECT c.id, c.chunk, u.vocab, u.vocab_interaction, u.unknown_vocab, c.sentencebreaks FROM chunks C
+        INNER JOIN user_nextchunk u
+        ON c.id = u.chunk_id
+        WHERE u.user_id = %s AND EXTRACT(DAY FROM u.next) <= EXTRACT(DAY FROM NOW()) 
+        """
+        cur.execute(COMMAND, (user,))
+        choices = cur.fetchall()
+    
+    else:
+        
+        COMMAND = """
+        SELECT c.id, c.chunk, u.vocab, u.vocab_interaction, u.unknown_vocab, c.sentencebreaks FROM chunks C
+        INNER JOIN user_nextchunk u
+        ON c.id = u.chunk_id
+        WHERE u.user_id = %s AND EXTRACT(DAY FROM u.next) <= EXTRACT(DAY FROM NOW()) and c.id=%s
+        """
+        cur.execute(COMMAND, (user, chunkid))
+        choices = cur.fetchall()
+    
+    out = {}
+
+    if not choices:
+        out["displayType"] = "done"
+        out["chunk"] = "No#new#reviews#left."
+        out["grammar"] = "0"
+    else:
+        choices = random.sample(choices, 1)
+        out["displayType"] = "sentence"
+        
+        chunk = choices[0][1].split("#")
+        
+        grammar = get_grammar(choices[0][0], cur)
+        print("grammar", grammar)
+        
+        vocab = get_vocab(choices[0][0], cur)
+        print("vocab", vocab)
+
+        vocab_to_test = choices[0][2].split(",")[next_interaction:]
+        print("to test", vocab_to_test)
+        
+        unknown_vocab = choices[0][4].split(",")
+        print("unknown vocab", unknown_vocab)
+        
+        sentencebreaks = choices[0][5].split("#")
+        print("sentencebreaks", sentencebreaks)
+                                             
+        
+        
+        # make the context dictionary
+        # w: word, g: grammar item, v: vocab item, u: unknown, t: test
+        
+        context = {}
+        for idx, word in enumerate(chunk):
+            context[str(idx)] = {'w': word, 'g': [], 'v': 0, 'u': 0, 't': 0}
+        for item in grammar:
+            for j in item[2].split(","):
+                context[j]['g'].append(str(item[0]))
+            
+        for item in vocab:
+            for j in item[2].split(","):
+                context[j]['v'] = str(item[0])
+                context[j]['vw'] = str(item[1])
+                context[j]['p'] = str(item[3])
+                if str(item[0]) in vocab_to_test:
+                    context[j]['t']=1
+        """
+        for item in vocab_to_test:
+            for idx in range(len(chunk)):
+                if item == context[str(idx)]['v']:
+                    context[str(idx)]['t']=1
+        """
+        # stringify context + sort out unknown vocab in context dict
+        
+        for key in context.keys():
+            if key in unknown_vocab:
+                context[key]['u'] = 1
+            context[key]['g'] = ','.join(context[key]['g'])
+            context[key]['v'] = str(context[key]['v'])
+        
+        print("context", context)
+        
+        # make dedicated grammar dictionary.
+        # ID is key: name, locations
+        
+        grammardict = {}
+        for item in grammar:
+            grammardict[item[0]] = {'n': item[1], 'l': item[2], 'u': 0, 't':0}
+        
+        out["context"] = context
+        out["grammar"] = grammardict
+        out["chunkid"] = choices[0][0]
+        
+        print(choices)
+        interactions = choices[0][3].split(",")[next_interaction:]
+        
+        # organise the vocab and interactions
+        
+        get_sentences_command = """SELECT sentences, locations FROM chunk_vocab WHERE chunk_id=%s AND vocab_id=%s"""
+        v_sentences = []
+        for v in vocab_to_test:
+            cur.execute(get_sentences_command, (choices[0][0], v))
+            records = cur.fetchall()
+            sentence = records[0][0].split(",")[0]
+            location = records[0][1].split(",")[0]
+            v_sentences.append((v, sentence, location))
+        v_sentences = sorted(v_sentences, key=lambda x: x[1])
+        
+        vocab_to_test = [x[0] for x in v_sentences]
+        lengths = [sentencebreaks[int(y[1])] for y in v_sentences]
+        locations = [x[2] for x in v_sentences]
+        
+        out["length"] = str(lengths[0])
+        
+        # get interaction data
+      
+        interactiondict = {}
+        vocab_interaction_data = get_vocab_interaction_data(choices[0][0], cur, vocab_to_test, interactions)
+        
+        for i, word in enumerate(vocab_to_test):
+            interactiondict[str(i)] = {}
+            interactiondict[str(i)]["mode"] = str(interactions[i])
+            interactiondict[str(i)]["location"] = str(locations[i])
+            interactiondict[str(i)]["v"] = word
+            interactiondict[str(i)]["length"] = str(lengths[i])
+            interactiondict[str(i)]["streak"] = vocab_interaction_data[i]["streak"]
+            if interactiondict[str(i)]["mode"] == "1":
+                for j, sen in enumerate(vocab_interaction_data[i]["raw"]):
+                    interactiondict[str(i)][str(j)] = {'s': sen[1], 'l': sen[2]}
+            if interactiondict[str(i)]["mode"] == "2":
+                for j, sen in enumerate(vocab_interaction_data[i]["raw"]):
+                    interactiondict[str(i)][str(j)] = {'s': sen[0]}
+            if interactiondict[str(i)]["mode"] == "3":
+                for j, sen in enumerate(vocab_interaction_data[i]["raw"]):
+                    interactiondict[str(i)][str(j)] = {'s': sen[0]}
+
+                    
+
+        out["currentInteraction"] = "0"
+        
+        print("interactiondict", interactiondict)
+        out["interaction"] = interactiondict
+
+        
+    return out
+
+def record_result(userid, req, cur):
+    
+    # get data
+
+    vocabid = req["interaction"][req["currentInteraction"]]["v"]
+    streak = req["interaction"][req["currentInteraction"]]["streak"]
+    chunkid = req["chunkid"]
+    correct = req["answeredCorrect"]
+    
+    if correct:
+        streak = int(streak) + 1
+    
+    # log the data
+   
+    COMMAND = """
+    INSERT INTO user_vocab_log (user_id, vocab_id, chunk_id, result)
+    VALUES (%s, %s, %s, %s)
+    """
+    cur.execute(COMMAND, (userid, vocabid, chunkid, correct))
+    
+    insert_command = """
+    INSERT INTO user_chunk_log (user_id, chunk_id)
+    VALUES (%s, %s)
+    """
+    cur.execute(insert_command, (userid, chunkid))
+    
+    # remove from user_nextchunk
+    
+    remove_chunk(userid, chunkid, cur)
+    
+    # pre-schedule in user_vocab
+    
+    COMMAND = """
+    UPDATE user_vocab
+    SET scheduled=0, next=(NOW() + INTERVAL '1 day'), streak=%s
+    WHERE user_id=%s AND vocab_id=%s
+    """
+    cur.execute(COMMAND, (str(streak), userid, vocabid))
+
+def remove_chunk(userid, chunkid, cur):
+    
+    remove_command = """
+    DELETE FROM user_nextchunk
+    WHERE user_id = %s AND chunk_id = %s
+    """
+    cur.execute(remove_command, (userid, chunkid))
+    
+def get_next(streak, correct):
+    
+    # will compute when next to test the vocab item, given whether correct and the current streak
+    
+    pass
